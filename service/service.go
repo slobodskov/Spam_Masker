cc9cc03ed3f1e1f96dfbc0de8f2c7ab4185271c4
slobodskov <148288578+slobodskov@users.noreply.github.com>
2025-06-11 00:18:29 +0300

    Update service.go

--- service/service.go
+++ service/service.go
@@ -20,40 +20,49 @@ func (s *Service) Run() error { //метод Run с использованием
 	if err != nil {
 		return err
 	}
+	inputChan := make(chan string, 10) //для избежания deadlock нужен буфер; также нужен для ограничения запущенных spamMasker
+	outputChan := make(chan string)
+	defer close(outputChan)
 
 	s.fileText = make([]string, len(data))
+
 	for i, line := range data {
-		s.fileText[i] = s.SpamMasker(line)
+		inputChan <- line
+		s.SpamMasker(inputChan, outputChan)
+		s.fileText[i] = <-outputChan
 	}
+
+	close(inputChan)
 
 	s.pres.Present(s.fileText)
 	return nil
 }
 
-func (s Service) SpamMasker(input string) string { //функция переделана в метод сервиса
+func (s Service) SpamMasker(inputChan <-chan string, outputChan chan<- string) { //функция переделана в метод сервиса
+	input := <-inputChan
+	go func() {
+		data := []byte(input)
+		size := len(data)
+		var link []byte
 
-	data := []byte(input)
-	size := len(data)
-	var link []byte
+		httpPrefix := []byte("http://")
+		httpPrefixLen := len(httpPrefix)
 
-	httpPrefix := []byte("http://")
-	httpPrefixLen := len(httpPrefix)
-
-	i := 0
-	for i < size {
-		if i <= size-httpPrefixLen && string(data[i:i+httpPrefixLen]) == string(httpPrefix) {
-			link = append(link, data[i:i+httpPrefixLen]...)
-			i += httpPrefixLen
-			for i < size && (data[i] != ' ' && data[i] != '\n' && data[i] != '\r') {
-				data[i] = '*'
+		i := 0
+		for i < size {
+			if i <= size-httpPrefixLen && string(data[i:i+httpPrefixLen]) == string(httpPrefix) {
+				link = append(link, data[i:i+httpPrefixLen]...)
+				i += httpPrefixLen
+				for i < size && (data[i] != ' ' && data[i] != '\n' && data[i] != '\r') {
+					data[i] = '*'
+					link = append(link, data[i])
+					i++
+				}
+			} else {
 				link = append(link, data[i])
 				i++
 			}
-		} else {
-			link = append(link, data[i])
-			i++
 		}
-	}
-
-	return string(link)
+		outputChan <- string(link)
+	}()
 }
